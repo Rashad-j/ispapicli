60ea9d424a2bc2abcefb0971329a291f8ab937f9
Rashad Jamara <rjamara@hexonet.net>
2020-04-29 15:53:16 +0200

    perf(core functionalities): added code to core functionalities

--- App/CLI/ispapi.py
+++ App/CLI/ispapi.py
@@ -10,51 +10,13 @@ import textwrap
 
 
 def main(args):
-    parser = argparse.ArgumentParser(
-        prog='ispapi',
-        add_help=False,
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description=textwrap.dedent('''\
-                ISPAPI - Commandline Tool
-            ---------------------------------------------
-                You can use the tool in two different ways:
-                One is by using '=' sign e.g. --command=QueryDomainList
-                Or by using spaces e.g. --command QueryDomainList
-            ----------------------------------------------
 
-            '''),
-        epilog=textwrap.dedent('''
-            You can use the command '--define <command>' to know everything about a specific command
-            You can also visit our documentation on https://github.com/hexonet/hexonet-api-documentation
-        ''')
-    )
-    parser.add_argument('--command', '-c', metavar='<command>',
-                        help='Enter a command e.g. -c=CheckDomain or -c CheckDomain')
-    parser.add_argument('--entity', '-e', choices={'live', 'ote'},
-                        help="set entity to either live or ote system e.g. -e=ote")
-    parser.add_argument('--userid', '-u', metavar='<user id>',
-                        help='Your login user ID')
-    parser.add_argument('--password', '-p', metavar='<your password>',
-                        help="Your login password")
-    parser.add_argument('--gui', '-g', const='gui', nargs='?', metavar='',
-                        help="Start graphical application")
-    parser.add_argument('--help', '-h', const='all', nargs='?', metavar='<command>',
-                        help="Show detailed use of a command")
-    parser.add_argument('args', nargs=argparse.REMAINDER,
-                        help='All additional args, e.g. limit=5')
-    parser.add_argument('--version', '-v', action='version',
-                        version='%(prog)s 2.0')
-    parser.error = errorFunction
-    original_args = ' '.join(args)
-    # remove spaces around the = cases ' =', '= ', ' = '
-    original_args = original_args.replace(" = ", "=")
-    original_args = original_args.replace(" =", "=")
-    original_args = original_args.replace("= ", "=")
-    splitted_args = original_args.split()
-    args = vars(parser.parse_args(splitted_args))
-    print(args)
-    reminderargs = args['args']
     core_obj = Core()
+    parser, splitted_args = core_obj.initParser(args)
+    # overwrite defualt error function with out local fuction
+    parser.error = errorFunction
+    args = vars(parser.parse_args(splitted_args))
+    reminderargs = args['args']
     try:
         parsed_params = core_obj.parseArgs(args, reminderargs)
         if type(parsed_params) == str:
@@ -62,7 +24,6 @@ def main(args):
                 startGUI()
             elif parsed_params == 'help':
                 parser.print_help()
-                pass
             else:
                 print(parsed_params)
         else:
--- App/CLI/modules/core.py
+++ App/CLI/modules/core.py
@@ -13,11 +13,53 @@ import time
 class Core:
     def __init__(self):
         # create private config list
-        self.command = ''
         self.cl = AC()
 
-    def initTool(self):
-        pass
+    def initParser(self, args):
+        parser = argparse.ArgumentParser(add_help=False)
+        parser.prog = 'ispapi'
+        parser.formatter_class = argparse.RawDescriptionHelpFormatter
+        parser.description = textwrap.dedent('''\
+                ISPAPI - Commandline Tool
+                ------------------------------------------------------------
+                The tool can be used in two modes:
+                 - By using '=' sign e.g. --command=QueryDomainList limit=5
+                 - By using spaces e.g. --command QueryDomainList limit 5
+                ------------------------------------------------------------
+
+                ''')
+        parser.epilog = textwrap.dedent('''
+                ------------------------------------------------------------
+                 You can use the command '--help <command>' to know everything about a specific command
+                 You can also visit our documentation on:
+                 https://github.com/hexonet/hexonet-api-documentation
+                ------------------------------------------------------------
+            ''')
+        parser.add_argument('--command', '-c', metavar='<command>',
+                            help='Enter a command e.g. -c=CheckDomain or -c CheckDomain')
+        parser.add_argument('--entity', '-e', choices={'live', 'ote'},
+                            help="set entity to either live or ote system e.g. -e=ote")
+        parser.add_argument('--userid', '-u', metavar='<user id>',
+                            help='Your login user ID')
+        parser.add_argument('--password', '-p', metavar='<your password>',
+                            help="Your login password")
+        parser.add_argument('--gui', '-g', const='gui', nargs='?', metavar='<>',
+                            help="Start graphical application")
+        parser.add_argument('--help', '-h', const='all', nargs='?', metavar='<command>,<>',
+                            help="Show detailed use of a 'command' OR use --help to show help")
+        parser.add_argument('args', nargs=argparse.REMAINDER,
+                            help='All additional args, e.g. limit=5')
+        parser.add_argument('--version', '-v', action='version',
+                            version='%(prog)s 2.0')
+        # clean extra spaces
+        original_args = ' '.join(args)
+        # remove spaces around the = cases are ' =', '= ', ' = '
+        original_args = original_args.replace(" = ", "=")
+        original_args = original_args.replace(" =", "=")
+        original_args = original_args.replace("= ", "=")
+        # split args in an array
+        splitted_args = original_args.split()
+        return parser, splitted_args
 
     def login(self, args, session_status=''):
         user = args['userid']
@@ -69,6 +111,9 @@ class Core:
 
     def parseArgs(self, args, parameters):
 
+        # if args['logout'] is not None:
+        #     self.logout()
+        #     return 'logout'
         if args['gui'] is not None:
             return 'gui'
         if args['help'] is not None:
